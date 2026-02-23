c1ae053b8d3c62bcfad226b38988da09997ad378
María del Mar García <116bmargar@gmail.com>
2026-02-23 17:54:39 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
- //Aquí tu código
+
+  const [name, setName] = useState('Sofía');
+  const [newName, setNewName]= useState('')
+  
+  return (
+    <div>
+      <h2>Teacher Name: {name}</h2>
+      <ul>
+        <li>
+            <button onClick={() => setName('Data')}>Data</button>
+        </li>
+        <li>
+            <button onClick={() => setName('Reyes')}>Reyes</button>
+        </li>
+        <li>
+            <button onClick={() => setName('Yolanda')}>Yolanda</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App
